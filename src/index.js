99dc28efb5de9c751bbc721127d9a519ac11a850
Dmitrii Ushakov <daushakov89@mail.ru>
2022-02-20 20:12:21 +0800

    feat: success

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, b) {
+
+  let arr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    
+    for (let j = 0; j < b.length; j++) {
+
+      if (b[j][0] != b[j][1]) {
+      
+        if (str[i] === b[j][0]) {
+          arr.push(b[j][0])
+          break
+        }
+
+        if (str[i] === b[j][1] && i === 0) {return false}
+  
+        if (str[i] === b[j][1] && arr.pop() != b[j][0]) {return false}
+      } 
+
+      if (b[j][0] === b[j][1])  {
+        
+        if (str[i] === b[j][0] && arr[arr.length-1] != b[j][0]) {
+          arr.push(b[j][0])
+          break
+        }
+
+        if (str[i] === b[j][0] && arr[arr.length-1] === b[j][0]){
+          arr.pop()
+        break
+        }
+        }
+
+      }
+    }
+  
+
+  return arr.length === 0 ? true : false
+
 }
